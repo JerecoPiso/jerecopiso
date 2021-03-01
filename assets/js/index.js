8303c8c313a629b8b2181dbfc32ce738d17f2402
JerecoPiso <jerecojamespiso@gmail.com>
2021-03-02 01:38:53 +0800

    dfDF

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,98 +1,158 @@
 $(document).ready(function(){
+ 
   $('#bars').click(function(){
     $('.links').slideDown();
     $('#bars').fadeOut()
   })
-   $('#close').click(function(){
+  $('#close').click(function(){
     $('.links').slideUp();
 
     $('#bars').fadeIn()
   })
-    $(this).scroll(function(){
-      
-      $('#skills').slideDown(1500);
-     $('#projects').fadeIn(4000);
-    });
+  $(this).scroll(function(){     
+    $('#skills').slideDown(1500);
+    $('#projects').fadeIn(4000);
+  });
 
-    $(".project-div").mouseenter(function(){
-          var view = $(this).attr("id");
-             $(this).children("img").css({"opacity": "0.5"});
-          //alert(view)
-          $('.view'+view).show();
+  $(".project-div").mouseenter(function(){
+      var view = $(this).attr("id");
+      $(this).children("img").css({"opacity": "0.5"});
+      //alert(view)
+     $('.view'+view).show();
+  });
 
-    });
-
-    $(".project-div").mouseleave(function(){
-          var view = $(this).attr("id");
-             $(this).children("img").css({"opacity": "1"});
-       
-          $('.view'+view).hide();
-    });
+  $(".project-div").mouseleave(function(){
+      var view = $(this).attr("id");
+      $(this).children("img").css({"opacity": "1"}) 
+      $('.view'+view).hide();
+  });
 
 
+    
+  $('#chatapp').click(function(){
+        $(".MyModalChatApp").slideDown();
 
+        $('.skills-projects, .div-container, #footer').css({'opacity': '0.5'})
 
-    var ctr = 0;
-    $(".carousel-images div").each(function(){
-      ctr++;
-    });
-    var ctr_two = 0;
-    $(".next").click(function(){
-        ctr_two++;
-        if($(".carousel-images div").last().hasClass('active')){
-              $('.active').hide();
-                $(".carousel-images div").first().addClass('active').show();
-              $('.active').last().removeClass('active');
-            
+          var ctr = 0;
+  $("#Chat-App .carousel-images div").each(function(){
+    ctr++;
+  });
+  var ctr_two = 0;
+  $(".next").click(function(){
+      ctr_two++;
+      if($("#Chat-App .carousel-images div").last().hasClass('active')){
+          $('#Chat-App .carousel-images .active').hide();
+          $("#Chat-App .carousel-images div").first().addClass('active').show();
+          $('#Chat-App .carousel-images .active').last().removeClass('active');         
+          ctr_two = 0;
+      }else{
+          $('#Chat-App .carousel-images .active').hide();     
+          $("#Chat-App .carousel-images .active").next().addClass('active').show()
+          $('#Chat-App .carousel-images .active').first().removeClass('active');
+            if(ctr_two == ctr){
+              $("#Chat-App .carousel-images div").first().addClass('active').show();
               ctr_two = 0;
-        }else{
-              $('.active').hide();
-         
-               $("div.active").next().addClass('active').show()
-              $('.active').first().removeClass('active');
-              if(ctr_two == ctr){
-                  $(".carousel-images div").first().addClass('active').show();
-                 ctr_two = 0;
-              }
+            }
         }
     });
       $(".previous").click(function(){
         
 
-        if($(".carousel-images div").first().hasClass('active')){
+        if($("#Chat-App .carousel-images div").first().hasClass('active')){
               ctr_two--;
-              $('.active').hide();
-              $('.active').last().removeClass('active');
+              $('#Chat-App .carousel-images .active').hide();
+              $('#Chat-App .carousel-images .active').last().removeClass('active');
           
-              $(".carousel-images div").last().addClass('active').fadeIn();
+              $("#Chat-App .carousel-images div").last().addClass('active').fadeIn();
         }else{
               ctr_two--;
-              $('.active').hide();
-              $("div.active").prev().addClass('active').show()         
-              $('.active').last().removeClass('active');
+              $('#Chat-App .carousel-images .active').hide();
+              $("#Chat-App .carousel-images .active").prev().addClass('active').show()         
+              $('#Chat-App .carousel-images .active').last().removeClass('active');
         }
         if(ctr_two == 0){
            ctr_two = ctr;
         }
     });
 
-    
-      $('.read-more').click(function(){
-        $(".MyModal").slideDown();
 
-        $('.skills-projects, .div-container, #footer').css({'opacity': '0.5'})
-
-      })
-      $('.close').click(function(){
+       $('.close').click(function(){
         
         set_ctr = 1;
-        $(".carousel-images div").removeClass("active")
-        $(".carousel-images div").first().addClass("active").show()
-        $(".MyModal").slideUp();
+        $("#Chat-App .carousel-images .carousel-images div").removeClass("active")
+        $("#Chat-App .carousel-images .carousel-images div").first().addClass("active").show()
+        $(".MyModalChatApp").slideUp();
         ctr_two = 0;
          $('.skills-projects, .div-container, #footer').css({'opacity': '1'})
       })
 
- $('.view-code').tooltip();
+    })
+
+
+
+
+  $('#loaning').click(function(){
+        $(".MyModalLoaning").slideDown();
+
+        $('.skills-projects, .div-container, #footer').css({'opacity': '0.5'})
+
+          var ctr = 0;
+  $("#Chat-App .carousel-images div").each(function(){
+    ctr++;
+  });
+  var ctr_two = 0;
+  $(".next").click(function(){
+      ctr_two++;
+      if($("#Loaning .carousel-images div").last().hasClass('active')){
+          $('#Loaning .carousel-images .active').hide();
+          $("#Loaning .carousel-images div").first().addClass('active').show();
+          $('#Loaning .carousel-images .active').last().removeClass('active');         
+          ctr_two = 0;
+      }else{
+          $('#Loaning .carousel-images .active').hide();     
+          $("#Loaning .carousel-images .active").next().addClass('active').show()
+          $('#Loaning .carousel-images .active').first().removeClass('active');
+            if(ctr_two == ctr){
+              $("#Loaning .carousel-images div").first().addClass('active').show();
+              ctr_two = 0;
+            }
+        }
+    });
+      $(".previous").click(function(){
+        
+
+        if($("#Loaning .carousel-images div").first().hasClass('active')){
+              ctr_two--;
+              $('#Loaning .carousel-images .active').hide();
+              $('#Loaning .carousel-images .active').last().removeClass('active');
+          
+              $("#Loaning .carousel-images div").last().addClass('active').fadeIn();
+        }else{
+              ctr_two--;
+              $('#Loaning .carousel-images .active').hide();
+              $("#Loaning .carousel-images .active").prev().addClass('active').show()         
+              $('#Loaning .carousel-images .active').last().removeClass('active');
+        }
+        if(ctr_two == 0){
+           ctr_two = ctr;
+        }
+    });
+
+
+       $('.close').click(function(){
+        
+        set_ctr = 1;
+        $("#Loaning .carousel-images .carousel-images div").removeClass("active")
+        $("#Loaning .carousel-images .carousel-images div").first().addClass("active").show()
+        $(".MyModalLoaning").slideUp();
+        ctr_two = 0;
+         $('.skills-projects, .div-container, #footer').css({'opacity': '1'})
+      })
+
+    })
+     
+
+ // $('.view-code').tooltip();
 
 }); 
