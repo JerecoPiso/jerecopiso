2e78e72faee6c0af4cb2e216197f0e033ef11d37
JerecoPiso <jerecojamespiso@gmail.com>
2021-03-31 16:10:35 +0800

    fgfg

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  	<title>JJ Online Portfoliosss</title>
+  	<title>JJ Online Portfolio</title>
 	  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
 	  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
